0a7cdc918866e41923d68ae7e4629f7171390a74
Brianna Escobar <briannaescobar6@gmail.com>
2023-05-11 15:56:13 -0400

    saas

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+import './components/style/App.scss';
+function App() {
+  return (
+   <>
+   <h1>1</h1>
+   <h2>2</h2>
+   <h3>3</h3>
+   <div className='tera'>
+    <h3>Earth</h3>
+    <ul>
+      <li> age: 4.5 billion years</li>
+      <li> radius 39588 miles</li>
+      <li> mass: 5972*10 to the 24th kg</li>
+    </ul>
+   </div>
+   <button className = "button"> 1 </button>
+   <button className = "button-prime"> 2 </button> 
+   </>
+  );
+}
+export default App;
+
+
+
+
+
+
+
+
+
+
